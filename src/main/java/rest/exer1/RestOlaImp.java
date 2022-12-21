d621bc7007e02a3d857e51c3da5eed5bcedad88c
Udinei <udineisilva@gmail.com>
2022-12-21 08:12:19 -0400

    first commit

---
+++ src/main/java/rest/exer1/RestOlaImp.java
@@ -0,0 +1,23 @@
+package rest.exer1;
+
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+// Rest é diferente do soap por podemos enviar qualquer tipo de texto para ser visto no navegador.
+// Por isso podemos testar os REST no navegador tambem. Soap envia o protocolo SOAP e por isso não da para
+// ver nada no navegador e nem executar.
+@Path("/ola")
+
+public class RestOlaImp implements Ola {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String servico() {
+        System.out.println("Executou o rest...");
+        return new Date().toString();
+    }
+}
